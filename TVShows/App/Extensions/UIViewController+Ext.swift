8730697dc643565a0badaaee5985e48efa2ebcaf
Diggo Silva <rdiggosilva@gmail.com>
2025-03-24 09:08:13 -0300

    Created Extensions files

--- TVShows/App/Extensions/UIViewController+Ext.swift
+++ TVShows/App/Extensions/UIViewController+Ext.swift
@@ -5,4 +5,13 @@
 //  Created by Diggo Silva on 23/03/25.
 //
 
-import Foundation
+import UIKit
+
+extension UIViewController {
+    func presentDSAlert(title: String, message: String, handler: ((UIAlertAction) -> Void)? = nil) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let ok = UIAlertAction(title: "Ok", style: .default, handler: handler)
+        alert.addAction(ok)
+        present(alert, animated: true)
+    }
+}
--- TVShows/App/Utils/UIView+Ext.swift
+++
@@ -1,8 +0,0 @@
-//
-//  UIView+Ext.swift
-//  TVShows
-//
-//  Created by Diggo Silva on 23/03/25.
-//
-
-import Foundation
