28c51d6faa52c26efbc55183e17fae797e477064
Diggo Silva <rdiggosilva@gmail.com>
2025-03-30 11:29:49 -0300

    refactored service file

--- TVShows/App/Network/Service.swift
+++ TVShows/App/Network/Service.swift
@@ -16,13 +16,73 @@ protocol ServiceProtocol {
 
 final class Service: ServiceProtocol {
     
-    func getShows(page: Int, completion: @escaping(Result<[Show], DSError>) -> Void) {        
-        guard let url = createURL(for: .page(page)) else { return }
+    func getShows(page: Int, completion: @escaping(Result<[Show], DSError>) -> Void) {
+        fetchData(endpoint: .page(page), decodingType: [ShowsResponse].self) { result in
+            switch result {
+            case .success(let showsResponse):
+                let shows = showsResponse.map { show in
+                    Show(id: show.id, name: show.name, image: show.image.medium, imageLarge: show.image.original, rating: show.rating.average)
+                }
+                completion(.success(shows))
+            case .failure(_):
+                completion(.failure(.showsFailed))
+            }
+        }
+    }
+    
+    func getCast(id: Int, completion: @escaping(Result<[Cast], DSError>) -> Void) {
+        fetchData(endpoint: .cast(id), decodingType: [CastResponse].self) { result in
+            switch result {
+            case .success(let castsResponse):
+                let casts = castsResponse.map { cast in
+                    Cast(id: cast.person.id, name: cast.person.name, image: (medium: cast.person.image.medium, original: cast.person.image.original), country: (name: cast.person.country?.name, code: cast.person.country?.code), birthday: cast.person.birthday, gender: cast.person.gender)
+                }
+                completion(.success(casts))
+            case .failure(_):
+                completion(.failure(.castFailed))
+            }
+        }
+    }
+
+    func getSeasons(id: Int, completion: @escaping(Result<[Season], DSError>) -> Void) {
+        fetchData(endpoint: .season(id), decodingType: [SeasonResponse].self) { result in
+            switch result {
+            case .success(let seasonsResponse):
+                let seasons = seasonsResponse.map { season in
+                    Season(id: season.id, number: season.number, image: (medium: season.image?.medium, original: season.image?.original), episodes: season.episodeOrder)
+                }
+                completion(.success(seasons))
+            case .failure(_):
+                completion(.failure(.seasonFailed))
+            }
+        }
+    }
+    
+    func getEpisodes(id: Int, completion: @escaping(Result<[Episode], DSError>) -> Void) {
+        fetchData(endpoint: .episodes(id), decodingType: [EpisodeResponse].self) { result in
+            switch result {
+            case .success(let episodesResponse):
+                let episodes = episodesResponse.map { episode in
+                    Episode(id: episode.id, name: episode.name, season: episode.season, number: episode.number, airdate: episode.airdate, airtime: episode.airtime, rating: episode.rating?.average, image: (medium: episode.image?.medium, original: episode.image?.original), summary: episode.summary)
+                }
+                completion(.success(episodes))
+            case .failure(_):
+                completion(.failure(.episodeFailed))
+            }
+        }
+    }
+    
+    private func fetchData<T: Decodable>(endpoint: TVMazeEndpoint, decodingType: T.Type, completion: @escaping(Result<T, DSError>) -> Void) {
+        guard let url = createURL(for: endpoint) else {
+            completion(.failure(.invalidData))
+            return
+        }
         
         let task = URLSession.shared.dataTask(with: url) { data, response, error in
             DispatchQueue.main.async {
-                if error != nil {
-                    completion(.failure(.showsFailed))
+                if let error = error {
+                    print("DEBUG: Erro ao fazer a requisição: \(error)")
+                    completion(.failure(.networkError))
                     return
                 }
                 
@@ -39,160 +99,11 @@ final class Service: ServiceProtocol {
                 }
                 
                 do {
-                    let showsResponse = try JSONDecoder().decode([ShowsResponse].self, from: data)
-                    var shows: [Show] = []
-                    
-                    for show in showsResponse {
-                        let show = Show(id: show.id,
-                                        name: show.name,
-                                        image: show.image.medium, imageLarge: show.image.original,
-                                        rating: show.rating.average)
-                        shows.append(show)
-                    }
-                    completion(.success(shows))
-                    
+                    let decodedResponse = try JSONDecoder().decode(decodingType, from: data)
+                    completion(.success(decodedResponse))
                 } catch {
+                    print("DEBUG: Erro ao decodificar os dados: \(error)")
                     completion(.failure(.failedDecoding))
-                }
-            }
-        }
-        task.resume()
-    }
-    
-    func getCast(id: Int, completion: @escaping(Result<[Cast], DSError>) -> Void) {
-        guard let url = createURL(for: .cast(id)) else { return }
-        
-        let task = URLSession.shared.dataTask(with: url) { data, response, error in
-            DispatchQueue.main.async {
-                if error != nil {
-                    completion(.failure(.castFailed))
-                    return
-                }
-                
-                guard let response = response as? HTTPURLResponse else {
-                    completion(.failure(.networkError))
-                    return
-                }
-                
-                print("DEBUG: Status code CAST: \(response.statusCode)")
-                
-                guard let data = data else {
-                    completion(.failure(.invalidData))
-                    return
-                }
-                
-                do {
-                    let castResponse = try JSONDecoder().decode([CastResponse].self, from: data)
-                    var casts: [Cast] = []
-                    
-                    for cast in castResponse {                        
-                        let cast = Cast(id: cast.person.id,
-                                        name: cast.person.name,
-                                        image: (cast.person.image.medium, cast.person.image.original),
-                                        country: (cast.person.country?.name, cast.person.country?.code),
-                                        birthday: cast.person.birthday,
-                                        gender: cast.person.gender)
-                        casts.append(cast)
-                    }
-                    completion(.success(casts))
-                    print("DEBUG: CAST: \(casts)")
-                    
-                } catch {
-//                    completion(.failure(.failedDecoding))
-                    print("DEBUG: CAST ERROR: \(error)")
-                }
-            }
-        }
-        task.resume()
-    }
-    
-    func getSeasons(id: Int, completion: @escaping(Result<[Season], DSError>) -> Void) {
-        guard let url = createURL(for: .season(id)) else { return }
-        
-        let task = URLSession.shared.dataTask(with: url) { data, response, error in
-            DispatchQueue.main.async {
-                if error != nil {
-                    completion(.failure(.seasonFailed))
-                    return
-                }
-                
-                guard let response = response as? HTTPURLResponse else {
-                    completion(.failure(.networkError))
-                    return
-                }
-                
-                print("DEBUG: Status code SEASON: \(response.statusCode)")
-                
-                guard let data = data else {
-                    completion(.failure(.invalidData))
-                    return
-                }
-                
-                do {
-                    let seasonResponse = try JSONDecoder().decode([SeasonResponse].self, from: data)
-                    var seasons: [Season] = []
-                    
-                    for season in seasonResponse {
-                        let season = Season(id: season.id,
-                                            number: season.number,
-                                            image: (season.image?.medium, season.image?.original),
-                                            episodes: season.episodeOrder)
-                        seasons.append(season)
-                    }
-                    completion(.success(seasons))
-                    print("DEBUG: SEASON: \(seasons)")
-                    
-                } catch {
-//                    completion(.failure(.failedDecoding))
-                    print("DEBUG: SEASON ERROR: \(error)")
-                }
-            }
-        }
-        task.resume()
-    }
-    
-    func getEpisodes(id: Int, completion: @escaping(Result<[Episode], DSError>) -> Void) {
-        guard let url = createURL(for: .episodes(id)) else { return }
-        
-        let task = URLSession.shared.dataTask(with: url) { data, response, error in
-            DispatchQueue.main.async {
-                if error != nil {
-                    completion(.failure(.episodeFailed))
-                    return
-                }
-                
-                guard let response = response as? HTTPURLResponse else {
-                    completion(.failure(.networkError))
-                    return
-                }
-                
-                print("DEBUG: Status code EPISODE: \(response.statusCode)")
-                
-                guard let data = data else {
-                    completion(.failure(.invalidData))
-                    return
-                }
-                
-                do {
-                    let episodeResponse = try JSONDecoder().decode([EpisodeResponse].self, from: data)
-                    var episodes: [Episode] = []
-                    
-                    for episode in episodeResponse {
-                        let episode = Episode(id: episode.id,
-                                              name: episode.name,
-                                              season: episode.season, number: episode.number,
-                                              airdate: episode.airdate, airtime: episode.airtime,
-                                              rating: episode.rating?.average,
-                                              image: (episode.image?.medium, episode.image?.original),
-                                              summary: episode.summary)
-                        episodes.append(episode)
-                    }
-                    completion(.success(episodes))
-                    print("DEBUG: EPISODES: \(episodes)")
-                    
-                } catch {
-//                    completion(.failure(.failedDecoding))
-                    print("DEBUG: EPISODE ERROR: \(error)")
                 }
             }
         }
