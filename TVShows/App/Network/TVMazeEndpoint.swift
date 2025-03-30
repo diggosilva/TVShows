66f7bc57abab40da98ca22bec745c4b448d73412
Diggo Silva <rdiggosilva@gmail.com>
2025-03-30 11:53:34 -0300

    TVMazeEndpoint file refactored

--- TVShows/App/Network/TVMazeEndpoint.swift
+++ TVShows/App/Network/TVMazeEndpoint.swift
@@ -7,32 +7,85 @@
 
 import Foundation
 
+//enum TVMazeEndpoint {
+//    case shows
+//    case page(Int)
+//    case id(Int)
+//    case cast(Int)
+//    case season(Int)
+//    case episodes(Int)
+//    
+//    var path: String {
+//        switch self {
+//        case .shows:
+//            return "/shows"
+//       
+//        case .page(_):
+//            return "/shows"
+//       
+//        case .id(let id):
+//            return "/shows/\(id)"
+//       
+//        case .cast(let id):
+//            return "/shows/\(id)/cast"
+//            
+//        case .season(let id):
+//            return "/shows/\(id)/seasons"
+//            
+//        case .episodes(let id):
+//            return "/shows/\(id)/episodes"
+//        }
+//    }
+//    
+//    var queryItems: [URLQueryItem]? {
+//        switch self {
+//        case .shows:
+//            return nil
+//            
+//        case .page(let page):
+//            return [URLQueryItem(name: "page", value: "\(page)")]
+//            
+//        case .id(_):
+//            return nil
+//            
+//        case .cast(_):
+//            return nil
+//            
+//        case .season(_):
+//            return nil
+//            
+//        case .episodes(_):
+//            return nil
+//        }
+//    }
+//}
+
 enum TVMazeEndpoint {
     case shows
-    case page(Int)
-    case id(Int)
-    case cast(Int)
-    case season(Int)
-    case episodes(Int)
+    case pagedShows(page: Int)
+    case showByID(id: Int)
+    case castForShow(id: Int)
+    case seasonsForShow(id: Int)
+    case episodesForShow(id: Int)
     
     var path: String {
         switch self {
         case .shows:
             return "/shows"
        
-        case .page(_):
+        case .pagedShows:
             return "/shows"
        
-        case .id(let id):
+        case .showByID(let id):
             return "/shows/\(id)"
        
-        case .cast(let id):
+        case .castForShow(let id):
             return "/shows/\(id)/cast"
             
-        case .season(let id):
+        case .seasonsForShow(let id):
             return "/shows/\(id)/seasons"
             
-        case .episodes(let id):
+        case .episodesForShow(let id):
             return "/shows/\(id)/episodes"
         }
     }
@@ -42,19 +95,19 @@ enum TVMazeEndpoint {
         case .shows:
             return nil
             
-        case .page(let page):
+        case .pagedShows(let page):
             return [URLQueryItem(name: "page", value: "\(page)")]
             
-        case .id(_):
+        case .showByID:
             return nil
             
-        case .cast(_):
+        case .castForShow:
             return nil
             
-        case .season(_):
+        case .seasonsForShow:
             return nil
             
-        case .episodes(_):
+        case .episodesForShow:
             return nil
         }
     }
