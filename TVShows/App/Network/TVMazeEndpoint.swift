e8c0e33c8c0d39678dc4ef1d4cd0fb7e0748a162
Diggo Silva <rdiggosilva@gmail.com>
2025-03-30 11:57:15 -0300

    Removed unused lines of code

--- TVShows/App/Network/TVMazeEndpoint.swift
+++ TVShows/App/Network/TVMazeEndpoint.swift
@@ -7,59 +7,6 @@
 
 import Foundation
 
-//enum TVMazeEndpoint {
-//    case shows
-//    case page(Int)
-//    case id(Int)
-//    case cast(Int)
-//    case season(Int)
-//    case episodes(Int)
-//    
-//    var path: String {
-//        switch self {
-//        case .shows:
-//            return "/shows"
-//       
-//        case .page(_):
-//            return "/shows"
-//       
-//        case .id(let id):
-//            return "/shows/\(id)"
-//       
-//        case .cast(let id):
-//            return "/shows/\(id)/cast"
-//            
-//        case .season(let id):
-//            return "/shows/\(id)/seasons"
-//            
-//        case .episodes(let id):
-//            return "/shows/\(id)/episodes"
-//        }
-//    }
-//    
-//    var queryItems: [URLQueryItem]? {
-//        switch self {
-//        case .shows:
-//            return nil
-//            
-//        case .page(let page):
-//            return [URLQueryItem(name: "page", value: "\(page)")]
-//            
-//        case .id(_):
-//            return nil
-//            
-//        case .cast(_):
-//            return nil
-//            
-//        case .season(_):
-//            return nil
-//            
-//        case .episodes(_):
-//            return nil
-//        }
-//    }
-//}
-
 enum TVMazeEndpoint {
     case shows
     case pagedShows(page: Int)
