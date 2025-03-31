94d5bb98be3264413254ee2b6e216dec3a5e799a
Diggo Silva <rdiggosilva@gmail.com>
2025-03-31 09:46:41 -0300

    XCode configuration

---
+++ TVShowsTests/DetailsViewModelTests.swift
@@ -0,0 +1,16 @@
+//
+//  DetailsViewModelTests.swift
+//  TVShowsTests
+//
+//  Created by Diggo Silva on 31/03/25.
+//
+
+import Testing
+
+struct DetailsViewModelTests {
+
+    @Test func <#test function name#>() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
